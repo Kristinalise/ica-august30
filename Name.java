e956a60f25a0b88f1799a2101db593289f9abaef
Kristinalise <87871816+Kristinalise@users.noreply.github.com>
2021-08-18 11:32:29 -0700

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,33 @@
+class Name {
+	
+	/* -----------------------------------------------------------------
+	 You can consult the Fruit.java handout as an example to help you with this ICA, 
+	 but first at least TRY to write Name.java without looking at Fruit.java.  
+	 ------------------------------------------------------------------*/
+
+	// 1.
+	// Declare three different INSTANCE VARIABLES for
+	// first, middle, and last names.  (What type should they be?)
+	// Make each variable "private."
+
+	// 2.
+	// Write TWO CONSTRUCTORS.
+	// One should have three parameters for first, middle, and last name instance variables.
+	// The other should have two parameters for first and last, and set middle to an empty String.
+	
+	// 3.
+	// Write GETTERS AND SETTERS for all three instance variables.
+	// Make all methods "public."
+	
+	// 4.
+	// Write a TOSTRING method.  It should also be "puplic."
+	// The TOSTRING should return, for example, "Kristin Alise Jones" or "Kristin Jones"
+	// depending on whether the middle name instance variable is empty or not.
+	// Hint: you can call the method .isEmpty() on a String variable, and it will return a boolean.
+
+	/* --------------------------------------------------------------
+	 When you are finished, COMPILE the Name class on the command line. 
+	 Then TEST your code by running java SwingTester on the command line.
+	-------------------------------------------------------------------*/
+
+}
